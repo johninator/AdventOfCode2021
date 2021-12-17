a7dfec895b333e8d9f82228ca36c620d92a130da
jho <jonas.hofmann@ipg-automotive.com>
2021-12-17 13:15:42 +0100

    add solution for day 8 part 1

--- day8/src/main.rs
+++ day8/src/main.rs
@@ -5,5 +5,11 @@ fn main() {
 
     let entries = read("input.txt").unwrap();
 
-    println!("{:?}", entries);
+
+    let counters : Vec<_> = entries.iter()
+        .map(|x| x[10..14].iter().filter(|&e| (e.len() < 5 || e.len() == 7)).count()).collect();
+    let result : usize = counters.iter().sum();
+
+    println!("{:?}", counters);
+    println!("{}", result);
 }
