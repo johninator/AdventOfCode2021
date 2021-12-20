45463b77e87619da2f51fa560893b125623b47d7
johninator <jonas.a.hofmann@web.de>
2021-12-20 10:23:41 +0100

    add solution for day 8 part 2

--- day8/src/main.rs
+++ day8/src/main.rs
@@ -1,5 +1,7 @@
 mod reader;
 use reader::read;
+use std::collections::HashMap;
+
 
 fn main() {
     let entries = read("input.txt").unwrap();
@@ -11,56 +13,134 @@ fn main() {
         let digits : Vec<_> = entry[10..14].to_vec();
 
         let digits = deduce_signals(&signals, &digits);
-        sum += digits.iter().sum::<i64>();
+        sum += digits[0] * 1000 + digits[1] * 100 + digits[2] * 10 + digits[3];
     }
     println!("sum: {}", sum);
 }
 
 fn deduce_signals(signals : &Vec<String>, digits : &Vec<String>) -> [i64; 4]
 {
-    let mut mapping : [i64; 10];
+    let mut mapping  = HashMap::new();
+
     let mut signals_sorted = signals.clone();
     signals_sorted.sort_by(|a,b| a.len().cmp(&b.len()));
 
-    for signal in signals_sorted
+    let mut cf = String::new();
+    let mut bd = String::new();
+    let mut a = String::new();
+    let mut g = String::new();
+
+    for i in 0..5 {
+
+    for signal in signals_sorted.clone()
     {
-        if signal.len() == 2 {
-
+        if signal.len() == 2 && i == 0 { // 1
+            cf = signal;
+            break;
+        } else if signal.len() == 3 && i == 1 {
+            // resolve 'a'
+            for ch in signal.chars() {
+                if !cf.contains(ch) {
+                    a.push(ch);
+                }
+            }
+            mapping.insert("a", a.chars().nth(0).unwrap());
+            break;
+        } else if signal.len() == 4  && i == 2 { // 7
+            for ch in signal.chars() {
+                if !cf.contains(ch) {
+                    bd.push(ch);
+                }
+            }
+            break;
+        } else if signal.len() == 5 && i == 3 && // 3
+                  signal.contains(cf.chars().nth(0).unwrap()) && 
+                  signal.contains(cf.chars().nth(1).unwrap())  {
+          // resolve 'b' & 'd'
+          let bd_0 = bd.chars().nth(0).unwrap();
+          let bd_1 = bd.chars().nth(1).unwrap();
+          if signal.contains(bd_0) {
+            mapping.insert("d", bd_0);
+            mapping.insert("b", bd_1);
+          } else {
+            mapping.insert("d", bd_1);
+            mapping.insert("b", bd_0);
+          }
+          break;
+        } else if signal.len() == 5 && i == 4 && // 5
+                 signal.contains(*mapping.get("b").unwrap())  {
+            // resolve 'c' & 'f'
+            let cf_0 = cf.chars().nth(0).unwrap();
+            let cf_1 = cf.chars().nth(1).unwrap();
+            if signal.contains(cf_0) {
+              mapping.insert("f", cf_0);
+              mapping.insert("c", cf_1);
+            } else {
+              mapping.insert("f", cf_1);
+              mapping.insert("c", cf_0);
+            }
+            // resolve 'g'
+            for ch in signal.chars() {
+                if !cf.contains(ch) &&
+                !bd.contains(ch) &&
+                !a.contains(ch) {
+                    g.push(ch);
+                }
+            }
+            mapping.insert("g", g.chars().nth(0).unwrap());
+            // resolve 'e'
+            let mut all = String::from("abcdefg");
+            for (key, val) in mapping.clone() {
+                if all.contains(val) {
+                  all = all.replace(val, "");
+                }
+            }
+            mapping.insert("e", all.chars().nth(0).unwrap());
+            break;
         }
-
-
-
-    }
-
-    return [0;4];
+   }
 }
 
-fn map_length_to_chars_digit(length : usize) -> Vec<Vec<char>>
-{
-    let mut chars = Vec::new();
+   let mut digits_result : [i64;4] = [0;4];
 
-    if length == 2 {
-        // 1
-        chars.push(vec!['c','f']);
-    } else if length == 3 {
-        // 7
-        chars.push(vec!['a','c','f']);
-    } else if length == 4 {
-        // 4
-        chars.push( vec!['b','c','d','f']);
-    } else if length == 7 {
-        // 8
-        chars.push( vec!['a','b','c','d','e','f','g']);
-    } else if length == 5 {
-        // 2,3,5
-        chars.push( vec!['a','c','d','e','g']);
-        chars.push( vec!['a','c','d','f','g']);
-        chars.push( vec!['a','b','d','f','g']);
-    } else {
-        // 9,0,6
-        chars.push( vec!['a','b','c','d','f','g']);
-        chars.push( vec!['a','b','c','e','f','g']);
-        chars.push( vec!['a','b','d','e','f','g']);
+   for (i,digit) in digits.iter().enumerate() {
+       let mut digit_resolved = String::new();
+       for char_digit in digit.chars() {
+        // resolve digit with mapping
+        for (key, val) in mapping.clone() {
+            if val == char_digit {
+                digit_resolved.push(key.chars().nth(0).unwrap());
+            }
+            
+        }
+       }
+
+       let mut digit_resolved_vec = digit_resolved.chars().collect::<Vec<char>>();
+       digit_resolved_vec.sort_unstable();
+       let digit_resolved_sorted : String = digit_resolved_vec.into_iter().collect();
+
+    if digit_resolved_sorted == "abcefd" {
+        digits_result[i] = 0;
+    } else if digit_resolved_sorted == "cf" {
+         digits_result[i] = 1;
+    } else if digit_resolved_sorted == "acdeg" {
+            digits_result[i] = 2;
+    } else if digit_resolved_sorted == "acdfg" {
+                digits_result[i] = 3;
+    } else if digit_resolved_sorted == "bcdf" {
+         digits_result[i] = 4;
+    } else if digit_resolved_sorted == "abdfg" {
+        digits_result[i] = 5;
+    } else if digit_resolved_sorted == "abdefg" {
+        digits_result[i] = 6;
+    } else if digit_resolved_sorted == "acf" {
+        digits_result[i] = 7;
+    } else if digit_resolved_sorted == "abcdefg" {
+        digits_result[i] = 8;
+    } else if digit_resolved_sorted == "abcdfg" {
+        digits_result[i] = 9;
     }
-    return chars;
+   }
+
+   return digits_result;
 }
